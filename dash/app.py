6ddf3e461b9fc3b5abcd0192974164be828643eb
Culver Stone <culver.stone57@gmail.com>
2024-11-22 20:35:39 -0800

    Started migrating to a multipage layout

--- dash/app.py
+++ dash/app.py
@@ -1,64 +1,17 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import plotly.express as px
+import dash
+from dash import Dash, html, dcc
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+app = Dash(__name__, use_pages=True)
 
-# Initialize the app - incorporate css
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(external_stylesheets=external_stylesheets)
-
-# App layout
-app.layout = [
-    html.Div(className='row', children='My First App with Data, Graph, and Controls',
-             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
-
-    html.Div(className='row', children=[
-        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
-                       value='lifeExp',
-                       inline=True,
-                       id='my-radio-buttons-final')
+app.layout = html.Div([
+    html.H1('Multi-page app with Dash Pages'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
     ]),
+    dash.page_container
+])
 
-    html.Div(className='row', children=[
-        html.Div(className='six columns', children=[
-            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
-        ]),
-        html.Div(className='six columns', children=[
-            dcc.Graph(figure={}, id='histo-chart-final')
-        ])
-    ])
-
-#    html.Div(children='My First App with Data, Graph, and Controls'),
-#    html.Hr(),
-
-#    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-#    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-#    dcc.Graph(figure={}, id='controls-and-graph')
-
-#    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
-]
-
-# Add controls to build the interaction
-#@callback(
-#    Output(component_id='controls-and-graph', component_property='figure'),
-#    Input(component_id='controls-and-radio-item', component_property='value')
-#)
-#def update_graph(col_chosen):
-#    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-#    return fig
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='histo-chart-final', component_property='figure'),
-    Input(component_id='my-radio-buttons-final', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
-
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
