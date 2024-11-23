77405e874bec514e0ad210ee9e84def78412b073
Culver Stone <culver.stone57@gmail.com>
2024-11-22 20:41:09 -0800

    Fixed the multi page dash, and merged the commit with the Dockerfile (didn't fix it)

--- dash/pages/original.py
+++ dash/pages/original.py
@@ -1,7 +1,10 @@
 # Import packages
+import dash
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+
+dash.register_page(__name__)
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
@@ -10,8 +13,8 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(external_stylesheets=external_stylesheets)
 
-# App layout
-app.layout = [
+
+layout = html.Div([
     html.Div(className='row', children='My First App with Data, Graph, and Controls',
              style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
 
@@ -30,25 +33,7 @@ app.layout = [
             dcc.Graph(figure={}, id='histo-chart-final')
         ])
     ])
-
-#    html.Div(children='My First App with Data, Graph, and Controls'),
-#    html.Hr(),
-
-#    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-#    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-#    dcc.Graph(figure={}, id='controls-and-graph')
-
-#    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
-]
-
-# Add controls to build the interaction
-#@callback(
-#    Output(component_id='controls-and-graph', component_property='figure'),
-#    Input(component_id='controls-and-radio-item', component_property='value')
-#)
-#def update_graph(col_chosen):
-#    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-#    return fig
+])
 
 # Add controls to build the interaction
 @callback(
@@ -58,7 +43,3 @@ app.layout = [
 def update_graph(col_chosen):
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
-
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
